4d2a4e204afbfcf3cf56455dabc0eabfe7cec566
Lucas Tinter <lucastinter07@gmail.com>
2025-12-02 14:16:34 -0500

    Rest Volleys, Render Arc, and Hit on Self Functions

--- SDP_Simulator/main.cpp
+++ SDP_Simulator/main.cpp
@@ -1,9 +1,15 @@
 #include "FEHLCD.h"
 #include "FEHUtility.h"
-#include <string>
+#include "FEHImages.h"
+#include <string.h>
+#include <math.h>
 using namespace std;
+#define g 9.81
+#define pi 3.14159265359
 
-// Button class
+// -------------------------
+// Button Class
+// -------------------------
 class Button {
 public:
     string label;
@@ -25,12 +31,13 @@ public:
     }
 };
 
-// touch handling function
+// -------------------------
+// Touch handling with debouncing
+// -------------------------
 void waitForTouch(float &x, float &y) {
     // Wait until a press is detected
     while (!LCD.Touch(&x, &y)) {}
     Sleep(0.15);  // debounce delay (150ms)
-
     // Wait until released to avoid double-detection
     while (LCD.Touch(&x, &y)) {}
 }
@@ -42,7 +49,7 @@ void mainMenu();
 void playMenu() {
     LCD.Clear();
     LCD.WriteAt("Play game here", 10, 10);
-
+    LCD.DrawCircle(40,40,5);
     Button back("Return to Menu", 10, 40, 180, 30);
     back.draw();
 
@@ -114,8 +121,6 @@ void creditsMenu() {
 void mainMenu() {
     LCD.Clear();
 
-    LCD.WriteAt("Tank Game!", 50, 10);
-
     Button play("Play", 50, 50, 100, 30);
     Button stats("Stats", 50, 90, 100, 30);
     Button inst("Instructions", 50, 130, 160, 30);
@@ -130,7 +135,6 @@ void mainMenu() {
 
     float x, y;
     waitForTouch(x, y);
-
     if (play.isPressed(x, y)) playMenu();
     else if (stats.isPressed(x, y)) statsMenu();
     else if (inst.isPressed(x, y)) instructionsMenu();
@@ -141,6 +145,37 @@ void mainMenu() {
         Sleep(1.0);
         return;
     }
+    else{}
+}
+
+// reset volleys
+// Initializes a variable to track the volley number and sets it to 5
+void resetVolleys(int &volleyCount) {
+    volleyCount=5;
+}
+
+// renders the arc of a projectile given the power and angle of a shot
+void renderProjectileArc(float x0, float y0, float power, float angleDeg) {
+    float angleRad=angleDeg*(pi/180.0);
+    float vx = power * cos(angleRad);
+    float vy = power * sin(angleRad);
+    for (float t = 0; t < 7.0; t += 0.05)
+    {
+        float x = x0 + vx * t;
+        float y = y0 - (vy * t - 0.5 * g * t * t); // minus because LCD y increases downward
+        if (x < 0 || x > 320 || y < 0 || y > 240) // Stop if arc goes offscreen
+            break;
+        LCD.DrawPixel(x, y);
+    }
+}
+
+bool wasHitOnSelf(float projX, float projY, float tankX, float tankY)
+{
+    const int TANK_width=40;
+    const int TANK_height=20;
+    bool withinX = (projX >= tankX) && (projX <= tankX + TANK_width);
+    bool withinY = (projY >= tankY) && (projY <= tankY + TANK_height);
+    return (withinX && withinY);
 }
 
 // execution block
@@ -149,3 +184,4 @@ int main() {
     mainMenu();  // start program
     return 0;
 }
+
